86f4b7130958fa72649c22441992061081c747b3
Psycho-Wolf <antony_pavel@hotmail.com>
2021-03-02 01:51:50 -0700

    comments!

--- Lab3/Lab3DataProcessing.m
+++ Lab3/Lab3DataProcessing.m
@@ -1,18 +1,19 @@
 clear
 clc
 
-data = load('AI.dat');
+data = load('AI.dat'); % Loads in the data from the LiDAR
 
-length = length(data);
-height = height(data);
+length = length(data); % Determines column number of the data
+height = height(data); % Determines row number of the data
 
-pointMat = zeros(3,length*16);
-[bbre1,bTe1] = scaraFK([-154.471221,38.942441,180.000000,115.528779]);
-[bbre2,bTe2] = scaraFK([-123.639568,120.409239,200.000000,183.230329]);
+pointMat = zeros(3,length*16); % Initializes an empty matrix to later be filled with the processed data
+[bbre1,bTe1] = scaraFK([-154.471221,38.942441,180.000000,115.528779]); % Determines BBrE and BTE from X,Y position 1
+[bbre2,bTe2] = scaraFK([-123.639568,120.409239,200.000000,183.230329]); % Determines BBrE and BTE from X,Y position 2
 
-
-i = 1;
+i = 1; % Interation count used in later for assigning the correct column
     for iter = 1:2:32
+        % IF statement that checks what row is currently being worked on
+        % and assigns the height to the correct value
         if (iter == 1) || (iter == 3)|| (iter == 17)|| (iter == 19)
             height = 0;
         elseif (iter == 5) || (iter == 7)|| (iter == 21)|| (iter == 23)
@@ -22,6 +23,8 @@ i = 1;
         elseif (iter == 13) || (iter == 15)|| (iter == 29)|| (iter == 31)
             height = 60;
         end    
+        % FOR Loop processes the data, bringing it from the LiDAR Frame to
+        % the SCARA Base Frame
         for ii = 1:length
             temp = [data(iter,ii)*cosd(data(iter+1,ii)), data(iter,ii)*sind(data(iter+1,ii)), height].';
             if (iter <= 16)
@@ -33,4 +36,5 @@ i = 1;
         end
 
     end
+% Plots the data in 3D
 plot3(pointMat(1,:),pointMat(2,:),pointMat(3,:),'.')
